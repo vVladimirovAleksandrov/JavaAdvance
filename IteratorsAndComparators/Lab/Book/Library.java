28ed4aab0e3fae2869d9f1a44c3708d5c4e7a337
Viktor Aleksandrov <94055679+vVladimirovAleksandrov@users.noreply.github.com>
2022-10-13 12:12:25 +0300

    Add files via upload

---
+++ IteratorsAndComparators/Lab/Book/Library.java
@@ -0,0 +1,31 @@
+package JavaAdvanced.IteratorsAndComparators.Lab.Book;
+
+import java.security.PublicKey;
+import java.util.Iterator;
+
+public class Library implements Iterable<Book> {
+    private Book[] books;
+
+    public Library(Book... books) {
+        this.books = books;
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+        return new LibraryIterator();
+    }
+
+    private class LibraryIterator implements Iterator<Book> {
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < books.length;
+        }
+
+        @Override
+        public Book next() {
+            return books[index++];
+        }
+    }
+}
